eb8a86abe27d8be116f3560c94efdd12ea693bfc
patrycjaniziol <patrycjaniziol97@gmail.com>
2023-02-02 19:15:38 +0100

    add data

--- wzjg.py
+++ wzjg.py
@@ -80,6 +80,49 @@ def random_colors(number_of_colors):
 
 warnings.filterwarnings('ignore')
 
+df0 = pd.read_csv('missforest1.csv', sep=',', encoding= 'unicode_escape')
+df = df0
+
+df = pd.DataFrame(df0)
+
+
+df['25-OH-Vitamin D'] = pd.to_numeric(df['25-OH-Vitamin D'],errors = 'coerce')
+df['albumin'] = pd.to_numeric(df['albumin'],errors = 'coerce')
+df['APTT'] = pd.to_numeric(df['APTT'],errors = 'coerce')
+df['protein (total)'] = pd.to_numeric(df['protein (total)'],errors = 'coerce')
+df['bilirubin (total)'] = pd.to_numeric(df['bilirubin (total)'],errors = 'coerce')
+df['ultra-sensitive CRP'] = pd.to_numeric(df['ultra-sensitive CRP'],errors = 'coerce')
+df['ferritin'] = pd.to_numeric(df['ferritin'],errors = 'coerce')
+df['folic acid'] = pd.to_numeric(df['folic acid'],errors = 'coerce')
+df['basophils %'] = pd.to_numeric(df['basophils %'],errors = 'coerce')
+df['basophils #'] = pd.to_numeric(df['basophils #'],errors = 'coerce')
+df['eosinophils %'] = pd.to_numeric(df['eosinophils %'],errors = 'coerce')
+df['eosinophils #'] = pd.to_numeric(df['eosinophils #'],errors = 'coerce')
+df['erythroblasts %'] = pd.to_numeric(df['erythroblasts %'],errors = 'coerce')
+df['erythroblasts #'] = pd.to_numeric(df['erythroblasts #'],errors = 'coerce')
+df['erythrocytes %'] = pd.to_numeric(df['erythrocytes %'],errors = 'coerce')
+df['HTC %'] = pd.to_numeric(df['HTC %'],errors = 'coerce')
+df['hemoglobin level'] = pd.to_numeric(df['hemoglobin level'],errors = 'coerce')
+df['leukocytes %'] = pd.to_numeric(df['leukocytes %'],errors = 'coerce')
+df['lymphocytes %'] = pd.to_numeric(df['lymphocytes %'],errors = 'coerce')
+df['lymphocytes #'] = pd.to_numeric(df['lymphocytes #'],errors = 'coerce')
+df['MCH'] = pd.to_numeric(df['MCH'],errors = 'coerce')
+df['MCHC'] = pd.to_numeric(df['MCHC'],errors = 'coerce')
+df['MCV'] = pd.to_numeric(df['MCV'],errors = 'coerce')
+df['monocytes %'] = pd.to_numeric(df['monocytes %'],errors = 'coerce')
+df['monocytes #'] = pd.to_numeric(df['monocytes #'],errors = 'coerce')
+df['MPV'] = pd.to_numeric(df['MPV'],errors = 'coerce')
+df['neutrophils %'] = pd.to_numeric(df['neutrophils %'],errors = 'coerce')
+df['neutrophils #'] = pd.to_numeric(df['neutrophils #'],errors = 'coerce')
+df['immature granulocytes #'] = pd.to_numeric(df['immature granulocytes #'],errors = 'coerce')
+df['P-LCR'] = pd.to_numeric(df['P-LCR'],errors = 'coerce')
+df['PCT'] = pd.to_numeric(df['PCT'],errors = 'coerce')
+df['PDW'] = pd.to_numeric(df['PDW'],errors = 'coerce')
+df['RDW-SD'] = pd.to_numeric(df['RDW-SD'],errors = 'coerce')
+df['RDW-CV'] = pd.to_numeric(df['RDW-CV'],errors = 'coerce')
+df['PT/INR'] = pd.to_numeric(df['PT/INR'],errors = 'coerce')
+df['potassium'] = pd.to_numeric(df['potassium'],errors = 'coerce')
+
 X = df.drop(["Mayo score", "gender"], axis=1)
 y = df["Mayo score"]
 
